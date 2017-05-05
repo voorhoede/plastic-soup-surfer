62341380ef98e5f19e7c855d2ad5259d90edf1c2
Servin Nissen <servin@Servins-MacBook-Pro.local>
2017-05-05 14:35:16 +0200

    feat(animation):warrants line + slide in

--- src/components/donation-progress/donation-progress.js
+++ src/components/donation-progress/donation-progress.js
@@ -4,16 +4,24 @@ const donationProgressVisual = document.querySelector(DONATION_PROGRESS__VISUAL)
 
 if (donationProgressVisual) {
     const donationProgressVisualTop = donationProgressVisual.offsetTop,
+            toppie = donationProgressVisual.scrollTop,
           documentHeight = window.innerHeight;
 
-    // if (window.innerWidth >= 768) {
-      window.addEventListener('scroll', function() {
-          if(donationProgressVisual.classList.contains('open')) return
-          let scrollFromTop = document.body.scrollTop || document.documentElement.scrollTop
+        //   alert(donationProgressVisualTop);
+        //   alert(toppie);
 
-          if (scrollFromTop >= donationProgressVisualTop - (documentHeight/3)*2) {
-              donationProgressVisual.classList.add('open')
-          }
-      })
-    // }
+    window.addEventListener('scroll', showWarrants)
+
+    function showWarrants() {
+        if(donationProgressVisual.classList.contains('open')) window.removeEventListener('scroll', showWarrants)
+
+        let scrollFromTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop,
+            bodyRect = document.body.getBoundingClientRect(),
+            elemRect = donationProgressVisual.getBoundingClientRect(),
+            offset   = elemRect.top - bodyRect.top;
+
+        if (scrollFromTop >= offset - (documentHeight/3)*2) {
+            donationProgressVisual.classList.add('open')
+        }
+    }
 }

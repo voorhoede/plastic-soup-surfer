68d7e735c2d7ebf7559bb3af6f264cbb32bb1b6b
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2017-09-21 16:01:10 +0200

    feat(donation-progress): set up new design for mobile

--- src/components/donation-progress/donation-progress.js
+++
@@ -1,8 +0,0 @@
-import scrollPastPoint from '../../scrollPastPoint';
-
-const DONATION_PROGRESS__VISUAL = '[data-donation-progress--visual]';
-
-const donationProgressVisual = document.querySelector(DONATION_PROGRESS__VISUAL)
-
-if (donationProgressVisual) 
-    scrollPastPoint(donationProgressVisual, 'open', 0.66)

5713137fd22d726ad90f690d2d944e1f75796836
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2017-11-07 14:44:26 +0100

    fix(map): prevent missing image from breaking icons placed on map

--- app/api/map.js
+++ app/api/map.js
@@ -88,7 +88,7 @@ module.exports = function (router, {liveStream, constants, nunjucksEnv}) {
         query.append('w', constants.mapOverlayImageWidth);
 
         return {
-            imageUrl : fields.image ? fields.image.fields.file.url + "?" + query : null,
+            imageUrl : fields.image.fields ? fields.image.fields.file.url + "?" + query : null,
             title    : fields.title,
             social   : null,
             body     : fields.description

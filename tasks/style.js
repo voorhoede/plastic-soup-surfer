b65bdfba339180b99605e4d565d60fc6aec69bcd
Selwyn <talk@selwyn.cc>
2018-05-14 17:01:06 +0200

    Fix CSS watch task building

--- tasks/style.js
+++ tasks/style.js
@@ -9,28 +9,33 @@ const gulpif = require('gulp-if');
 const postcss = require('gulp-postcss');
 const postcssLess = require('postcss-less');
 
-gulp.task('style', function () {
+function buildStyle() {
     const environment = process.env.NODE_ENV;
 
     return gulp.src(process.env.SRC_DIR + '/main.less')
-		.pipe(plumber())
-		.pipe(gulpif(environment !== 'production', sourcemaps.init()))
-		.pipe(less({
+        .pipe(plumber())
+        .pipe(gulpif(environment !== 'production', sourcemaps.init()))
+        .pipe(less({
             paths : [process.env.SRC_DIR]
         }))
-		.on('error', function(error) {
-			console.error(error.message);
-			this.emit('end');
-		})
-		.pipe(autoprefixer({ browsers: ['> 1%', 'last 2 versions'] }))
-		.pipe(gulpif(environment === 'production', cleanCSS()))
-		.pipe(gulpif(environment !== 'production', sourcemaps.write('./')))
-		.pipe(gulp.dest(process.env.DIST_DIR + '/assets/css/'));
-});
+        .on('error', function(error) {
+            console.error(error.message);
+            this.emit('end');
+        })
+        .pipe(autoprefixer({ browsers: ['> 1%', 'last 2 versions'] }))
+        .pipe(gulpif(environment === 'production', cleanCSS()))
+        .pipe(gulpif(environment !== 'production', sourcemaps.write('./')))
+        .pipe(gulp.dest(process.env.DIST_DIR + '/assets/css/'));
+}
+
+gulp.task('style', buildStyle);
 
 gulp.task('style:format', function () {
       return gulp.src(process.env.SRC_DIR + '/**/*.less', { base: './' })
-        .pipe(postcss([cssDeclarationSorter({order: 'smacss'})], { syntax: postcssLess }))
+        .pipe(postcss(
+            [cssDeclarationSorter({order: 'smacss'})],
+            { syntax: postcssLess }
+        ))
         .pipe(gulp.dest('.'));
 });
 
@@ -38,7 +43,29 @@ if(process.env.NODE_ENV === 'development') {
     const watch = require('gulp-watch');
 	gulp.task('style:watch', ['style'], function () {
 		return watch(process.env.SRC_DIR  + '/**/*.less', () => {
-			gulp.start('style');
-		});
+            clearLessCacheHack();
+            buildStyle();
+        });
 	});
+}
+
+function clearLessCacheHack() {
+    const gulpLessVersion = require('../package-lock.json').dependencies['gulp-less'].version;
+
+    if (gulpLessVersion !== '4.0.0') {
+        throw new Error(`
+            This is a temporary hacky workaround to solve:
+            https://github.com/less/less.js/issues/3185
+            Remove when the issue is solved.
+        `)
+    }
+
+    const less = require('less');
+    const fileManagers = less.environment && less.environment.fileManagers || [];
+
+    fileManagers.forEach(fileManager => {
+        if (fileManager.contents) {
+            fileManager.contents = {};
+        }
+    });
 }

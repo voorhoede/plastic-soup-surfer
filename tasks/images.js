74cb25c9caec1c4a22a8091ee6268fa33f1f231f
Selwyn <talk@selwyn.cc>
2018-05-16 17:05:59 +0200

    Initial restyle of the past adventures

--- tasks/images.js
+++ tasks/images.js
@@ -110,19 +110,19 @@ gulp.task('images', function() {
 
             'assets/images/adventure--message-on-a-bottle.jpg' : [
                 {
-                    width:660
+                    width: 299 * 2,
                 }
             ],
 
             'assets/images/adventure--plastic-hunters.jpg' : [
                 {
-                    width:660
+                    width: 299 * 2,
                 }
             ],
 
             'assets/images/adventure--plastic-soup-board.jpg' : [
                 {
-                    width:660
+                    width: 299 * 2,
                 }
             ],
 

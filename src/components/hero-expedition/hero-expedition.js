4a23437f43522e694a3af2b21706042468e53579
Selwyn <talk@selwyn.cc>
2018-05-23 14:10:31 +0200

    Reduce hero variants with Nunjucks macro

--- src/components/hero-expedition/hero-expedition.js
+++
@@ -1,8 +0,0 @@
-import scrollPastPoint from '../../scrollPastPoint';
-
-const HERO__PLASTIC_SURFER_IMAGE = '[data-hero--image]';
-
-const heroPlasticSurferImage = document.querySelector(HERO__PLASTIC_SURFER_IMAGE);
-
-if (heroPlasticSurferImage)
-    scrollPastPoint(heroPlasticSurferImage, 'open', .5)
